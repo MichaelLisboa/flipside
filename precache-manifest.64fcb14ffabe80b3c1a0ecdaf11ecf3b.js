cd7824fcc8502764434941036df7401ba4bcbb29
Michael <michael@pixt.us>
2019-10-08 11:22:22 +0700

    Updates

---
+++ precache-manifest.64fcb14ffabe80b3c1a0ecdaf11ecf3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8220609bd143ef0ed0d111c147dde633",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d2727aa3e9307e322b7",
+    "url": "/static/css/main.0a53ac3d.chunk.css"
+  },
+  {
+    "revision": "f608094511d4a40cac58",
+    "url": "/static/js/2.72de4b6c.chunk.js"
+  },
+  {
+    "revision": "1d2727aa3e9307e322b7",
+    "url": "/static/js/main.4f76f53a.chunk.js"
+  },
+  {
+    "revision": "588d018485264c7d5c89",
+    "url": "/static/js/runtime~main.9da1ac9d.js"
+  },
+  {
+    "revision": "8601b15a26b4c160ad745a46e1b60a8a",
+    "url": "/static/media/flipside-logo.8601b15a.svg"
+  }
+]);
