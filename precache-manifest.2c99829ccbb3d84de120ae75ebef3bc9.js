f4def2aacb0bbe932876959187123b01b486a91c
Michael <michael@pixt.us>
2019-10-08 14:43:17 +0700

    Updates

--- precache-manifest.2c99829ccbb3d84de120ae75ebef3bc9.js
+++ precache-manifest.2c99829ccbb3d84de120ae75ebef3bc9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3d194e510cb3b7558aef19c604a33ba1",
+    "revision": "a822a1a233e734078a45bf0ca27d9a29",
     "url": "/flipside/index.html"
   },
   {
-    "revision": "31a628765a67d7617433",
-    "url": "/flipside/static/css/main.d9084c21.chunk.css"
+    "revision": "c0284d6208b200af1bdb",
+    "url": "/flipside/static/css/main.4925bb16.chunk.css"
   },
   {
     "revision": "f608094511d4a40cac58",
     "url": "/flipside/static/js/2.72de4b6c.chunk.js"
   },
   {
-    "revision": "31a628765a67d7617433",
-    "url": "/flipside/static/js/main.07525005.chunk.js"
+    "revision": "c0284d6208b200af1bdb",
+    "url": "/flipside/static/js/main.fe6a83dd.chunk.js"
   },
   {
     "revision": "8e86d1c34ed13c44ba9f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flipside/precache-manifest.6bf29c244f9ea6dd7929ac03d3f2eb43.js"
+  "/flipside/precache-manifest.2c99829ccbb3d84de120ae75ebef3bc9.js"
 );
 
 self.addEventListener('message', (event) => {
